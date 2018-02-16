45055578a5804ca937fbd30fc905b46e6f0ed767
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-02-16 21:10:13 +1100

    Add GrayDecode

--- gray_code/Program.cs
+++ gray_code/Program.cs
@@ -8,9 +8,19 @@ namespace gray_code
 {
     public class Gray
     {
-        public static ulong grayEncode(ulong n)
+        public static ulong GrayEncode(ulong n)
         {
             return n ^ (n >> 1);
+        }
+
+        public static ulong GrayDecode(ulong n)
+        {
+            ulong i = 1 << 8 * 64 - 2; //long is 64-bit
+            ulong p, b = p = n & i;
+
+            while ((i >>= 1) > 0)
+                b |= p = n & i ^ p >> 1;
+            return b;
         }
     }
 }
