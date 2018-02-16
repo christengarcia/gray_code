448e4d6915a1ce920a0800656233b9ea79fec697
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-02-16 21:08:08 +1100

    Initial commit

---
+++ gray_code/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * C# Program to Create a Gray Code
+ */
+
+using System;
+
+namespace gray_code
+{
+    public class Gray
+    {
+        public static ulong grayEncode(ulong n)
+        {
+            return n ^ (n >> 1);
+        }
+    }
+}
